a61d71ad814cc82805705da89a9483fd92e1fed0
Aruljothi Parthiban <aruljothiparthiban@hotmail.com>
2022-11-08 11:22:15 +0530

    provider fix

--- src/product/product.routes.js
+++ src/product/product.routes.js
@@ -4,6 +4,5 @@ const controller = require('./product.controller');
 router.get("/product/:id", controller.getById);
 router.get("/products", controller.getAll);
 router.post("/products", controller.create);
-router.get("/:baseSiteId/product/:productCode", controller.getProductByCode);
 
 module.exports = router;
